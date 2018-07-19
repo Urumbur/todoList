b519af3460ebd4f2ceba9b1832771e29e8a24e1b
amadry <adrian.madry@a2cc.eu>
2018-07-19 09:17:00 +0200

    cashe

--- assets/scripts/sw.js
+++ assets/scripts/sw.js
@@ -1,13 +1,13 @@
-// const CACHE_NAME = 'app-cache';
+const CACHE_NAME = 'app-cache';
  
-// const urlsToCache = [
-//   'index.html',
-//   'style.css',
-//   'assets/scripts/newscript.js',
-// ];
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'assets/scripts/newscript.js',
+];
 
 self.addEventListener('install', (event) => {
-    // event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-    //     return cache.addAll(urlsToCache);
-    // }));
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(urlsToCache);
+    }));
 });
